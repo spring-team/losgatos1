e52fce3600d95c1662fb7373685ebd215d5f1d02
Rod Johnson <johnsonroda@gmail.com>
2018-02-20 11:17:12 +1100

    Update Losgatos1Controller.java

--- src/main/java/c/a/Losgatos1Controller.java
+++ src/main/java/c/a/Losgatos1Controller.java
@@ -11,7 +11,7 @@ class Losgatos1Controller {
 
     @RequestMapping(method = GET, path = "/")
     public String root() {
-        return "App running: Served from " + getClass().getName();
+        return "App still running: Served from " + getClass().getName();
     }
 
     @RequestMapping(method = GET, path = "hello/{name}")
