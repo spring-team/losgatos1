2800661387a1f8c810ff5a4052b02bacd3ddf57b
Rod Johnson <johnsonroda@gmail.com>
2018-02-27 20:54:01 -0800

    Everyone needs encouragement to write Java

--- src/main/java/c/a/Losgatos1Application.java
+++ src/main/java/c/a/Losgatos1Application.java
@@ -14,3 +14,5 @@ public class Losgatos1Application {
 As I say yes to life, life says yes to me.
 
 Deep at the center of my being is an infinite well of love.
+
+Deep at the center of my being is an infinite well of love.
