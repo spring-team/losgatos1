e86a34811be121dfc5ba8a67f6a337df40e1ec81
Rod Johnson <johnsonroda@gmail.com>
2018-03-02 15:12:22 -0600

    Everyone needs encouragement to write Java

--- src/main/java/c/a/Losgatos1Application.java
+++ src/main/java/c/a/Losgatos1Application.java
@@ -1,3 +1,5 @@
+// You are the delta in what you do, not the things you did in the past
+
 // It’s only a thought, and a thought can be changed.
 
 // It’s only a thought, and a thought can be changed.
