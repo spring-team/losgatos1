3ed75da1195b675c010af2c9284cb6c70cb2e84e
Rod Johnson <johnsonroda@gmail.com>
2018-03-02 19:28:53 -0600

    Everyone needs encouragement to write Java

--- src/main/java/c/a/Losgatos1Application.java
+++ src/main/java/c/a/Losgatos1Application.java
@@ -1,3 +1,5 @@
+// I come from the loving space of my heart, and I know that love opens all doors.
+
 // You are the delta in what you do, not the things you did in the past
 
 // It’s only a thought, and a thought can be changed.
