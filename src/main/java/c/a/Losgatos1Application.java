56904f982c674ee8855ec2129b828ccfe7ed6963
Rod Johnson <johnsonroda@gmail.com>
2018-03-25 23:39:07 +1100

    Everyone needs encouragement to write Java

--- src/main/java/c/a/Losgatos1Application.java
+++ src/main/java/c/a/Losgatos1Application.java
@@ -1,3 +1,5 @@
+// I believe in you. You can do the thing!
+
 // Deep at the center of my being is an infinite well of love.
 
 // You are the delta in what you do, not the things you did in the past
