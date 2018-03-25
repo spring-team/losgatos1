fbd820a57227637f64aad33ccd750d49366f4845
Rod Johnson <johnsonroda@gmail.com>
2018-03-25 23:59:55 +1100

    Everyone needs encouragement to write Java

--- src/main/java/c/a/Losgatos1Application.java
+++ src/main/java/c/a/Losgatos1Application.java
@@ -1,5 +1,7 @@
 // It’s only a thought, and a thought can be changed.
 
+// It’s only a thought, and a thought can be changed.
+
 // I believe in you. You can do the thing!
 
 // Deep at the center of my being is an infinite well of love.
