73ab52e019d0130d42d7cd54c98827931777a966
Rod Johnson <johnsonroda@gmail.com>
2018-03-25 21:07:02 +1100

    Everyone needs encouragement to write Java

--- src/main/java/c/a/Losgatos1Application.java
+++ src/main/java/c/a/Losgatos1Application.java
@@ -1,3 +1,5 @@
+// You're good enough, you're smart enough, and doggone it, people like you.
+
 // It’s only a thought, and a thought can be changed.
 
 // You're good enough, you're smart enough, and doggone it, people like you.
