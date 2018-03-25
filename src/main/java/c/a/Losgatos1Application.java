ee8cc5daf456add4a493cce4742000e27cee5e38
Rod Johnson <johnsonroda@gmail.com>
2018-03-25 23:00:29 +1100

    Everyone needs encouragement to write Java

--- src/main/java/c/a/Losgatos1Application.java
+++ src/main/java/c/a/Losgatos1Application.java
@@ -1,5 +1,7 @@
 // I believe in you. You can do the thing!
 
+// I believe in you. You can do the thing!
+
 // I come from the loving space of my heart, and I know that love opens all doors.
 
 // You are the delta in what you do, not the things you did in the past
