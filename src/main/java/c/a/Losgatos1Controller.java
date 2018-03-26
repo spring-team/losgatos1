6822629d691fc85c7a39c90df80849ed14166ab1
Rod Johnson <johnsonroda@gmail.com>
2018-03-26 16:56:29 +1100

    Everyone needs encouragement to write Java

--- src/main/java/c/a/Losgatos1Controller.java
+++ src/main/java/c/a/Losgatos1Controller.java
@@ -1,3 +1,5 @@
+// You are the delta in what you do, not the things you did in the past
+
 // It’s only a thought, and a thought can be changed.
 
 // Deep at the center of my being is an infinite well of love.
