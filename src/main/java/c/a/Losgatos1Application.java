fbd531ab5cfbea45fa7d76feac8d40ceb60a48d0
Rod Johnson <johnsonroda@gmail.com>
2018-03-27 15:38:09 +1100

    Everyone needs encouragement to write Java

--- src/main/java/c/a/Losgatos1Application.java
+++ src/main/java/c/a/Losgatos1Application.java
@@ -1,5 +1,7 @@
 // Deep at the center of my being is an infinite well of love.
 
+// Deep at the center of my being is an infinite well of love.
+
 // It’s only a thought, and a thought can be changed.
 
 // I believe in you. You can do the thing!
