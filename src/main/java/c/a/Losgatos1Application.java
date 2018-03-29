c693471fe7bbb46f9aff14e03ae5ca66df6371ea
Rod Johnson <johnsonroda@gmail.com>
2018-03-29 11:02:42 +1100

    Everyone needs encouragement to write Java

--- src/main/java/c/a/Losgatos1Application.java
+++ src/main/java/c/a/Losgatos1Application.java
@@ -1,3 +1,5 @@
+// You're good enough, you're smart enough, and doggone it, people like you.
+
 // It’s only a thought, and a thought can be changed.
 
 // As I say yes to life, life says yes to me.
