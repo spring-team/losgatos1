0f6d9f69f573207867877757defb6eaaed1834c4
Rod Johnson <johnsonroda@gmail.com>
2018-04-09 16:43:19 +1000

    Squirrels

--- src/main/java/Thing.java
+++ src/main/java/Thing.java
@@ -13,5 +13,5 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-// Squirrel number 1523256118830 gnawed industriously
+// Squirrel number 1523256196230 gnawed industriously
 public class Thing {}
