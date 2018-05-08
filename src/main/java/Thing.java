a56aa3a92974f4c328da5bbf33cada721953999a
Rod Johnson <johnsonroda@gmail.com>
2018-05-08 13:24:32 -0700

    Everyone needs encouragement to write Java

--- src/main/java/Thing.java
+++ src/main/java/Thing.java
@@ -1,3 +1,5 @@
+// Finite games are played within boundaries. Infinite games play with boundaries.
+
 // Make new mistakes as fast as you can.
 
 // Finite games are played within boundaries. Infinite games play with boundaries.
