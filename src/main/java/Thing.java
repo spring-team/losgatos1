1a03e2876c036adc24e6c3d3151a8141d6015ac4
Rod Johnson <johnsonroda@gmail.com>
2018-05-07 18:36:36 -0700

    Everyone needs encouragement to write Java

--- src/main/java/Thing.java
+++ src/main/java/Thing.java
@@ -1,3 +1,5 @@
+// It’s only a thought, and a thought can be changed.
+
 // No need to say 'I can do it.' You ARE doing it!
 
 // You're good enough, you're smart enough, and doggone it, people like you.
