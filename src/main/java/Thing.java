4c285649652dc2d47c4cf2355a811d5b705b82e1
Rod Johnson <johnsonroda@gmail.com>
2018-05-07 17:44:17 -0700

    Everyone needs encouragement to write Java

--- src/main/java/Thing.java
+++ src/main/java/Thing.java
@@ -1,3 +1,5 @@
+// You're good enough, you're smart enough, and doggone it, people like you.
+
 // Finite games are played within boundaries. Infinite games play with boundaries.
 
 // Our bodies and minds are capable of far more than our psyche will let us achieve.
