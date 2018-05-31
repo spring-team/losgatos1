7b16e44524608eb68aede391f1555eddffa1bdba
Rod Johnson <johnsonroda@gmail.com>
2018-05-31 12:25:28 +1000

    Everyone needs encouragement to write Java

--- src/main/java/Thing.java
+++ src/main/java/Thing.java
@@ -2,6 +2,8 @@
 
 // Life is its own reason for being. It is beautiful to have a sense of beauty.
 
+// Life is its own reason for being. It is beautiful to have a sense of beauty.
+
 // I believe in you. You can do the thing!
 
 // Life is its own reason for being. It is beautiful to have a sense of beauty.
